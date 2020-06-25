c37af684db6f8dad8399bb5ac92f566ec6c632ec
karumajohn <karumajohn252203@gmail.com>
2020-06-25 15:38:52 +0300

    Initial commit

---
+++ app/src/main/java/com/trichain/androidwidget/MainActivity.java
@@ -0,0 +1,13 @@
+package com.trichain.androidwidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
